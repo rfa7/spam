380be831279c31f4b0496893b4058ecc309329f4
Rafal Pietrykowski <rfa@gazeta.pl>
2016-01-13 18:08:42 +0100

    dodanie zmiennych a-l

--- 12kul.py
+++ 12kul.py
@@ -2,10 +2,22 @@
 gra 12 KUL - 3 wazenia
 
 mamy 12 kul, a-l, dzielimy je na 3 grupy
-
+'''
 kulki = {
 'a':2, 'b':2,'c':2,'d':2,'e':1,'f':2,'g':2,'i':2,'j':2,'k':2,'l':2
 }
 
-print a + b
+print kulki['a'] + kulki['b']
+a = 2
+b = 2
+c = 2
+d = 2
+e = 2
+f = 2
+g = 2
+h = 2
+i = 2
+j = 2
+k = 2
+l = 2
 
