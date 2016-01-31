3b04e4d0c8f508a479e9cb9a68fac1f41de1ae74
Rafal Pietrykowski <rfa@gazeta.pl>
2016-01-31 07:40:26 +0100

    bisec_search1.py

---
+++ testy/bisec_search1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+zapytaj o liczbę
+poproś o określenie czy wskazana liczba spełnia oczekiwania, czy jest też za
+duża lub za mała
+'''
+
+print('Pomyśl liczbę od 0 do 100')
+min = 0
+max = 100
+ans = (min+max)/2
+
+def pytanie():
+    x = str(raw_input('Czy liczba ' + str(ans) + ' jest właściwa? T-tak, D-za duża, M-za mała: '))
+    return x
+x = pytanie()
+while x != 'd'or x != 'm' or x != 't':
+    print('Podano niewłaściwą odpowiedź - możliwe T,D,M. Spróbuj ponownie')
+if x  == 'T':
+    print('OK')
+elif x == 'D':
+    print('xxxxxD')
+    min = ans
+elif x =='M':
+    print('asdfasdfaM')
+    max = ans
+else:
+    pytanie()
