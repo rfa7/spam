6dd78c549b0928b0b90bf195993f1e9059ce0a22
rfa7 <rfa@gazeta.pl>
2017-05-13 12:48:01 +0200

    Execute file function run_file(file_name, opt=dir)

---
+++ run_file.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.insert(0,'e:\\dev\\python')
+
+# function run/execute given file
+def run_file(file, dir = ''):
+    '''
+    if len(dir_in_python) > 0:    
+        if dir_in_python[0] != '\\':
+            dir_in_python = '\\' + dir_in_python
+    '''
+    #if file[0] != '\\':
+    #    file = '\\' + file
+    my_path = os.path.join('e:\\dev\\python\\',dir, file)
+    print(my_path)
+    return exec(open(my_path).read())
+
