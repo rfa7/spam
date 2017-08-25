30657c40767346d29513a22f148b2a6e394d3a2c
Rafal Pietrykowski <rfa@gazeta.pl>
2017-08-25 10:37:14 +0200

    deleted unnecesery comments

--- run_file.py
+++ run_file.py
@@ -9,8 +9,6 @@ def run_file(file, dir = ''):
         if dir_in_python[0] != '\\':
             dir_in_python = '\\' + dir_in_python
     '''
-    #if file[0] != '\\':
-    #    file = '\\' + file
     my_path = os.path.join('e:\\dev\\python\\',dir, file)
     print(my_path)
     return exec(open(my_path).read())
